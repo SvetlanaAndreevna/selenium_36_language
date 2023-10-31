8e2b40bba27dec0ae39cfee047efb13ae0653266
Svetlana <KalatinaSveta09@yandex.ru>
2023-10-31 09:45:03 +0300

    Исправлен селектор для выбора кнопки корзины

--- test_items.py
+++ test_items.py
@@ -4,6 +4,7 @@ import time
 def test_add_to_basket(browser):
     browser.implicitly_wait(5)
     browser.get('https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    basket = browser.find_elements(By.ID, 'id_quantity')
+    basket = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
+    # basket[0].click() # при желании можно проверить и добавить товар в корзину
     time.sleep(5) #добавила, чтобы посмотреть
-    assert basket, 'Кнопка добавления в корзину не найдена'
+    assert len(basket) == 1, 'Кнопка добавления в корзину не найдена' # проверяем, что кнопка найдена и что она одна
